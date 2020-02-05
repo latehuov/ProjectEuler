e78e59653b9245819c7b0378c80210549cb38678
lasse PC <t8hula00@students.oamk.fi>
2020-02-05 18:28:40 +0200

    finished problem2 in kt

--- kt solutions/problem2.kt
+++ kt solutions/problem2.kt
@@ -1,12 +1,20 @@
+import java.util.Arrays
 fun main(){
     var fibSum: Int = 0
-    var fibArray: Array
-    do{
-        if(fib.rem(2)==0){
-            fibSum = fibSum + fib
-        }
+    var curFib: Int = 0
+    var prevFib1: Int = 1
+    var prevFib2: Int = 0
+    while(curFib < 4000000){
 
-        println("fib:   $fib;")
-        println("fibSum: $fibSum")
-    }while(newFib<4000000)
+        curFib = prevFib1 + prevFib2
+        println(curFib)
+
+        prevFib2 = prevFib1
+        prevFib1 = curFib
+
+        if (curFib.rem(2)==0){
+            fibSum = fibSum + curFib
+        }
+    }
+    println("sum of even fibonacci numbers under 4,000,000: $fibSum")
 }
